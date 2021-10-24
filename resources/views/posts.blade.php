f1ed5f18b4b43466f31b7fee48ae608a300a7bc3
WeiChuanLin <weichuan5626@gmail.com>
2021-10-25 01:51:37 +0800

    blade layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
-
+<x-layout>
     @foreach ($posts as $post)
 
         <article>
@@ -26,6 +15,4 @@
 
         </article>
     @endforeach
-    
-</body>
-</html>
+</x-layout>
