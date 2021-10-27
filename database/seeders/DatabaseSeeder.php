8cd3336fb68c68f3c6a5ca4b0b59494f9476b2d1
WeiChuanLin <weichuan5626@gmail.com>
2021-10-28 03:04:33 +0800

    建立 PostFactory 與 CategoryFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,49 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Post::truncate();
-        User::truncate();
-        Category::truncate();
-
-        $user = \App\Models\User::factory()->create();
-        $u = \App\Models\User::factory()->create();
-
-
-        $personal = Category::create([
-            'name'=>'Personal',
-            'slug'=>'personal'
-        ]);
-
-        $work = Category::create([
-            'name'=>'Work',
-            'slug'=>'work'
-        ]);
-
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$personal->id,
-            'title'=>'Family Post',
-            'slug'=>'family-post',
-            'excerpt'=>'lorem',
-            'body'=>'loremloremlorem'
-        ]);
-
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$work->id,
-            'title'=>'Work Post',
-            'slug'=>'work-post',
-            'excerpt'=>'lorem',
-            'body'=>'loremloremlorem'
-        ]);
-
-        Post::create([
-            'user_id'=>$u->id,
-            'category_id'=>$personal->id,
-            'title'=>'Personal Post',
-            'slug'=>'personal-post',
-            'excerpt'=>'lorem',
-            'body'=>'loremloremlorem'
-        ]);
+        Post::factory(5)->create();
     }
 }
