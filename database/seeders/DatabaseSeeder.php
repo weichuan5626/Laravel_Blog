8008916e3c877f936e862d5624037c7d0f79392c
WeiChuanLin <weichuan5626@gmail.com>
2021-10-28 02:31:48 +0800

    Database:Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Post::truncate();
+        User::truncate();
+        Category::truncate();
+
+        $user = \App\Models\User::factory()->create();
+        $u = \App\Models\User::factory()->create();
+
+
+        $personal = Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal'
+        ]);
+
+        $work = Category::create([
+            'name'=>'Work',
+            'slug'=>'work'
+        ]);
+
+        Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$personal->id,
+            'title'=>'Family Post',
+            'slug'=>'family-post',
+            'excerpt'=>'lorem',
+            'body'=>'loremloremlorem'
+        ]);
+
+        Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$work->id,
+            'title'=>'Work Post',
+            'slug'=>'work-post',
+            'excerpt'=>'lorem',
+            'body'=>'loremloremlorem'
+        ]);
+
+        Post::create([
+            'user_id'=>$u->id,
+            'category_id'=>$personal->id,
+            'title'=>'Personal Post',
+            'slug'=>'personal-post',
+            'excerpt'=>'lorem',
+            'body'=>'loremloremlorem'
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
@@ -37,5 +38,12 @@ Route::get('/categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('/users/{user}', function (User $user) {
+
+    return view('posts', [
+        'posts' => $user->posts
     ]);
 });
