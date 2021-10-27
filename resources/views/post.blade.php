0043211c610edc9ca1143d20c9788a0db93bf050
WeiChuanLin <weichuan5626@gmail.com>
2021-10-28 01:41:02 +0800

    透過 category 查看該 category 底下的文章

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,9 @@
         <h1>
             {{ $post->title }}
         </h1>
+        <p>
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </p>
         <div>
             <p>
                 {!! $post->body !!}
