2579c9c90bcf842a24748ad394ff396adfb84ef8
WeiChuanLin <weichuan5626@gmail.com>
2021-10-31 14:28:42 +0800

    Convert the Blog Post Page

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -9,14 +9,12 @@
         <div class="flex-1 flex flex-col justify-between">
             <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
-                    <a href="/categories/{{ $post->category->slug }}"
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{ $post->category->name }}</a>
+                    <x-category-button :category="$post->category" />
                 </div>
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        <a href="/posts/{{ $post->slug }}"></a>{{ $post->title }}
+                        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
