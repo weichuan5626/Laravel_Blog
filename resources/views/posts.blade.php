21ce6320a6e9401a9b05adf554935e7393b8b9e4
WeiChuanLin <weichuan5626@gmail.com>
2021-10-31 11:08:55 +0800

    HTML & CSS

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,21 @@
 <x-layout>
-    @foreach ($posts as $post)
+    @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+    </main>
+    {{-- @foreach ($posts as $post)
 
         <article>
 
@@ -18,5 +34,5 @@
             </div>
 
         </article>
-    @endforeach
+    @endforeach --}}
 </x-layout>
