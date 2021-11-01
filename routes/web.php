4a328894533d743c92f45f79cbeed9977c88563f
WeiChuanLin <weichuan5626@gmail.com>
2021-11-02 01:48:56 +0800

    Search (The Messy Way)

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,16 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest();
+
+    if(request('search')){
+        $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
 
     return view('posts', [
-        'posts' => Post::latest()->with(['category', 'author'])->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
 })->name('home');
