f4918a666817ee86819ff4911925331c397c4939
WeiChuanLin <weichuan5626@gmail.com>
2021-11-02 02:13:10 +0800

    Search (The Cleaner Way)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,11 +14,24 @@ class Post extends Model
 
     protected $fillable = ['category_id', 'title', 'slug', 'excerpt', 'body'];
 
-    public function category(){
+    public function scopeFilter($query, $filters)
+    {
+        $query->when(
+            $filters['search'] ?? false,
+            fn ($query, $search) =>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search  . '%')
+        );
+    }
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function author(){
+    public function author()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
 }
--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -38,7 +38,7 @@
 
                 <div>
                     <a href="/posts/{{ $post->slug }}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8 whitespace-nowrap"
                     >Read More</a>
                 </div>
             </footer>
