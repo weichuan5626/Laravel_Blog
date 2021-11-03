62a528dbef86e5e1ca35d16751e77f669b3680c2
WeiChuanLin <weichuan5626@gmail.com>
2021-11-03 14:38:59 +0800

    Fix a Confusing Eloquent Query Bug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,12 +16,15 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-        $query = $query->when(
+        $query->when(
             $filters['search'] ?? false,
             fn ($query, $search) =>
             $query
-                ->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search  . '%')
+                ->where(fn ($query) =>
+                $query
+                    ->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('body', 'like', '%' . $search  . '%'))
+
         );
 
         $query->when(
