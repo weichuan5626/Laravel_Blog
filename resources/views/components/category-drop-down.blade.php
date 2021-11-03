00864bb454494ef94a8f9e744ebe6535f6fc4cf9
WeiChuanLin <weichuan5626@gmail.com>
2021-11-03 14:13:22 +0800

    Merge Category and Search Queries

--- resources/views/components/category-drop-down.blade.php
+++ resources/views/components/category-drop-down.blade.php
@@ -10,6 +10,9 @@
     <x-dropdown-item href="/" :active='!isset($currentCategory)' >All</x-dropdown-item>
 
     @foreach ($categories as $category)
-        <x-dropdown-item href="?category={{ $category->slug }}" :active="isset($currentCategory) ? $currentCategory->slug===$category->slug  : false" >{{ $category->name }}</x-dropdown-item>
+        <x-dropdown-item 
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}" 
+            :active="isset($currentCategory) ? $currentCategory->slug===$category->slug : false" 
+        >{{ $category->name }}</x-dropdown-item>
     @endforeach
 </x-dropdown>
